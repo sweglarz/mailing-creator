7e92a9ab535570d4b003e4eeeb5cf3bb33831139
Szymon Węglarz <simon.weglarz@gmail.com>
2021-09-20 19:11:04 +0200

    cosmetic changes in javascript

--- javascript/main.js
+++ javascript/main.js
@@ -1,38 +1,38 @@
-const imageElement = document.querySelector(".js-creationImage");
-let htmlString;
+{
+    const imageElement = document.querySelector(".js-creationImage");
 
-const checkSizeOfUploadedImage = () => {
-    const imageBox = document.querySelector(".js-box");
+    const checkSizeOfUploadedImage = () => {
+        const imageBox = document.querySelector(".js-box");
 
-    imageElement.onload = () => {
-        if (imageElement.height > "900") {
-            imageBox.classList.add("creation__box--biggerImage");
-        } else {
-            imageBox.classList.remove("creation__box--biggerImage")
-        }
+        imageElement.addEventListener("load", () => {
+            if (imageElement.height > 700) {
+                imageBox.classList.add("creation__box--biggerImage");
+            } else {
+                imageBox.classList.remove("creation__box--biggerImage")
+            }
+        });
     }
-}
 
-const addNewImage = (event) => {
-    event.preventDefault();
-    const inputElement = document.querySelector(".js-creationInput");
-    const [file] = inputElement.files;
-    if (file) {
-        imageElement.src = URL.createObjectURL(file)
-    };
-    checkSizeOfUploadedImage();
-}
+    const addNewImage = (event) => {
+        event.preventDefault();
+        const inputElement = document.querySelector(".js-creationInput");
+        const [file] = inputElement.files;
+        if (file) {
+            imageElement.src = URL.createObjectURL(file)
+        };
+        checkSizeOfUploadedImage();
+    }
 
-const creationInit = () => {
-    const creationForm = document.querySelector(".js-creationForm");
-    creationForm.addEventListener("submit", addNewImage);
-}
-creationInit();
+    const creationInit = () => {
+        const creationForm = document.querySelector(".js-creationForm");
+        creationForm.addEventListener("submit", addNewImage);
+    }
+    creationInit();
 
-const chooseBase = (base, urlHolder, altHolder) => {
+    const chooseBase = (base, urlHolder, altHolder) => {
 
-    if (base.value === "wp") {
-        htmlString = `
+        if (base.value === "wp") {
+            return htmlString = `
 <html>
 <head>
     <title>WP</title>
@@ -51,12 +51,12 @@ const chooseBase = (base, urlHolder, altHolder) => {
         </table>
     </body>
 </html>`
-    } else if (base.value === "onet") {
-        htmlString =
-            `<html>
+        } else if (base.value === "onet") {
+            return htmlString =
+                `<html>
 <head>
     <title>ONET</title>
-    <meta http-equiv="Content-Type" content="text/html" charset="ISO-8859-2">
+    <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
     <body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
 </head>
     <body>
@@ -71,12 +71,12 @@ const chooseBase = (base, urlHolder, altHolder) => {
         </table>
     </body>
 </html>`
-    } else {
-        htmlString =
-            `<html>
+        } else {
+            return htmlString =
+                `<html>
 <head>
     <title>INTERIA</title>
-    <meta http-equiv="Content-Type" content="text/html" charset="ISO-8859-2">
+    <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
     <body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
 </head>
     <body>
@@ -91,49 +91,49 @@ const chooseBase = (base, urlHolder, altHolder) => {
         </table>
     </body>
 </html>`
+        }
     }
-}
 
-const prepareImage = () => {
-    let imagePath = imageElement.getAttribute("src");
-    let fileName = "1.jpg";
+    const prepareImage = () => {
+        let imagePath = imageElement.src;
+        let fileName = "1.jpg";
 
-    saveAs(imagePath, fileName);
-}
-
-const prepareIndex = () => {
-    const urlHolder = document.querySelector(".js-url").value;
-    const altHolder = document.querySelector(".js-alt").value;
-    const base = document.querySelector(".js-base");
-    chooseBase(base, urlHolder, altHolder);
-    clearInputs(urlHolder, altHolder);
-
-    var blob = new Blob([htmlString], { type: "text/plain;charset=utf-8" });
-    saveAs(blob, "index.html");
-}
-
-const downloadCreation = () => {
-    prepareImage()
-    prepareIndex();
-}
-
-const showMessage = () => {
-    const alert = document.querySelector(".js-alert");
-    if (imageElement.src === "https://sweglarz.github.io/mailing-creator/img/background.jpg") {
-        alert.textContent = "Musisz przesłać kreację!"
-    } else {
-        alert.textContent = "";
-        downloadCreation();
+        saveAs(imagePath, fileName);
     }
-}
 
-const onFormSubmit = (event) => {
-    event.preventDefault();
-    showMessage();
-}
+    const prepareIndex = () => {
+        const urlHolder = document.querySelector(".js-url").value;
+        const altHolder = document.querySelector(".js-alt").value;
+        const base = document.querySelector(".js-base");
+        chooseBase(base, urlHolder, altHolder);
 
-const settingsInit = () => {
-    const settingsForm = document.querySelector(".js-settingsForm");
-    settingsForm.addEventListener("submit", onFormSubmit);
+        const blob = new Blob([htmlString], { type: "text/plain;charset=utf-8" });
+        saveAs(blob, "index.html");
+    }
+
+    const downloadCreation = () => {
+        prepareImage()
+        prepareIndex();
+    }
+
+    const showMessage = () => {
+        const alert = document.querySelector(".js-alert");
+        if (imageElement.src === "https://sweglarz.github.io/mailing-creator/img/background.jpg") {
+            alert.textContent = "Musisz przesłać kreację!"
+        } else {
+            alert.textContent = "";
+            downloadCreation();
+        }
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        showMessage();
+    }
+
+    const settingsInit = () => {
+        const settingsForm = document.querySelector(".js-settingsForm");
+        settingsForm.addEventListener("submit", onFormSubmit);
+    }
+    settingsInit();
 }
-settingsInit();
